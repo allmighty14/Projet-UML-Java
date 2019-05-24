4d2920ebb60cbbd3e13d001973e830e58673c294
williamsiewe <loicosiewe16@gmail.com>
2019-05-24 20:51:21 +0100

    first commit

--- BoulderDash/src/main/java/com/ucac_icam/BoulderDash/Main.java
+++ BoulderDash/src/main/java/com/ucac_icam/BoulderDash/Main.java
@@ -24,7 +24,8 @@ public abstract class Main {
      */
 	 public static void main(final String[] args) {
       
-		 /* final Model model = new Model();
+		System.out.println("all");
+		/* final Model model = new Model();
         final View view = new View(model);
         final Controller controller = new Controller(view, model);
         view.setController(controller);
